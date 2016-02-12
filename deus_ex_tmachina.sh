6ac8bcc3b28e0633565bc98fc874113fec67f587
Fabrice Laporte <kraymer@gmail.com>
2016-02-12 23:18:42 +0100

    open list in editor and save output script on disk

--- deus_ex_tmachina.sh
+++ deus_ex_tmachina.sh
@@ -1,2 +1,16 @@
-ls -d /*/ | grep -v /Users/ | tr '\n' '\n' | while read line; do echo "sudo tmutil addexclusion -p $line"; done
-ls -d $HOME/*/  | tr '\n' '\n' | while read line; do echo "sudo tmutil addexclusion -p $line"; done
+#!/bin/bash
+
+out=`mktemp`.sh
+echo ${out}
+echo "#!/bin/bash
+
+# comment the lines for directories you want to back up
+" > ${out}
+exclude_line='while read line; do echo "sudo tmutil addexclusion -p \"$line\"";done'
+ls -d ${HOME}/*/  | tr '\n' '\n' | eval ${exclude_line} >> ${out}
+ls -d /*/ | grep -v -E "/(Users|dev|net|home)/" | tr '\n' '\n' | eval ${exclude_line} >> ${out}
+echo "echo 'TM exclusions list updated'" >> ${out}
+
+editor=${EDITOR:-${VISUAL:-vi}}
+${editor} ${out}
+chmod +x ${out}
