3cc4c8ef446e6e51c0a5cc401f96044e2e10675e
Fabrice <kraymer@gmail.com>
2024-04-07 17:59:02 +0200

    Include hidden directories in listing

--- deus_ex_tmachina.sh
+++ deus_ex_tmachina.sh
@@ -7,7 +7,7 @@ echo "#!/bin/bash
 # comment the lines for directories you want to back up
 " > ${out}
 exclude_line='while read line; do echo "sudo tmutil addexclusion -p \"$line\"";done'
-ls -d ${HOME}/*/  | tr '\n' '\n' | eval ${exclude_line} >> ${out}
+ls -d -- ${HOME}/.*/ ${HOME}/*/ | tr '\n' '\n' | eval ${exclude_line} >> ${out}
 ls -d /*/ | grep -v -E "/(Users|dev|net|home)/" | tr '\n' '\n' | eval ${exclude_line} >> ${out}
 echo "echo 'TM exclusions list updated'" >> ${out}
 
